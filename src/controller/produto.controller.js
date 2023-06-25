9207add091437cdc4a2ce8d12549dc7f81062d34
Daniel Kulka <kulkad13@gmail.com>
2023-06-25 17:52:50 -0300

    Feat: produto CRUD completo

---
+++ src/controller/produto.controller.js
@@ -0,0 +1,60 @@
+const produtoService = require("../service/produto.service");
+
+const findProductByIdController = async (req,res) => {
+  try{
+    return res.send(await produtoService.findProductByIdService(req.params.id));
+  }catch(err){
+    console.log(`erro: ${err.message}`);
+    return res.status(500).send({ message: `Erro inesperado, tente novamente!`});
+  }
+};
+
+const findAllProductsController = async (req,res) => {
+  try{
+    return res.send(await produtoService.findAllProductsService());
+  }catch(err){
+    console.log(`erro: ${err.message}`);
+    return res.status(500).send({ message: `Erro inesperado, tente novamente!`});
+  }
+};
+
+const createProductController = async (req,res) => {
+  try{
+    const corpo = {
+      ...req.body,
+      userId: req.userId,
+      createdAt: new Date()
+    }
+
+    return res.send(await produtoService.createProductService(corpo));
+  }catch(err){
+    console.log(`erro: ${err.message}`);
+    return res.status(500).send({ message: `Erro inesperado, tente novamente!`});
+  }
+};
+
+const updateProductController = async (req,res) => {
+  try{
+    return res.send(await produtoService.updateProductService(req.params.id, req.body));
+  }catch(err){
+    console.log(`erro: ${err.message}`);
+    return res.status(500).send({ message: `Erro inesperado, tente novamente!`});
+  }
+};
+
+const deleteProductController = async (req,res) => {
+  try{
+    return res.send(await produtoService.deleteProductService(req.params.id));
+  }catch(err){
+    console.log(`erro: ${err.message}`);
+    return res.status(500).send({ message: `Erro inesperado, tente novamente!`});
+  }
+};
+
+module.exports = {
+  findProductByIdController,
+  findAllProductsController,
+  createProductController,
+  updateProductController,
+  deleteProductController
+}
