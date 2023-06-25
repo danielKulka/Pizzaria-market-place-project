0b9499f05537044ea82d499a656b5c77dbd4f1ac
Daniel Kulka <kulkad13@gmail.com>
2023-06-24 23:17:05 -0300

    Feat: Add + Rm User Address

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -85,6 +85,14 @@ const removeUserController = async (req, res) => {
 
 const addUserAddressController = async (req, res) => {
   try{
+    req.body.createdAt = new Date();
+    const endereco = await userService.addUserAddressService(req.params.id, req.body);
+
+    if(endereco.ok == 1){
+      return res.status(201).send({ message: `Endereço adicionado com sucesso!`});
+    }else{
+      return res.status(400).send({ message: `Algo deu errado no endereço, tente novamente`});
+    }
 
   }catch(err){
     console.log(`erro: ${err.message}`);
@@ -94,6 +102,13 @@ const addUserAddressController = async (req, res) => {
 
 const removeUserAddressController = async (req, res) => {
   try{
+    const endereco = await userService.removeUserAddressService(req.body.id, req.body.addressId);
+    
+    if(endereco.ok == 1){
+      return res.status(200).send({ message: `Endereço removido com sucesso!`});
+    }else{
+      return res.status(400).send({ message: `Algo deu errado no endereço, tente novamente`});
+    }
 
   }catch(err){
     console.log(`erro: ${err.message}`);
