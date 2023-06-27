5fba5ce5b5503f4f895eb4aae4b2e51eea7920a5
Daniel Kulka <kulkad13@gmail.com>
2023-06-27 11:12:59 -0300

    Feat: valida login implementado

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const auttController = require("../controller/auth.controller");
+const { validaLogin } = require("../middleware/validacao.middleware");
 
-router.post("/login", auttController.loginController);
+router.post("/login", validaLogin, auttController.loginController);
 
 module.exports = router;
